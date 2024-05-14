cfba13a40ef44ee75857065d13ca6a6e68825b27
LeonelGlezHdez <160064590+LeonelGlezHdez@users.noreply.github.com>
2024-05-14 08:09:43 +0000

    Version 2

--- makefile
+++ makefile
@@ -1,5 +1,7 @@
-bin/tui :  src/tui_test.cpp
-	g++ src/tui_test.cpp -o bin/tui -I include -lftxui-screen -lftxui-dom -lftxui-component -std=c22
 
-run : bin/tui
+
+bin/tui : src/tui_test.cpp
+	 g++ src/tui_test.cpp -o bin/tui -I include -lftxui-screen -lftxui-dom -l ftxui-component -std=c++2a
+
+run : bin/tui|
 	./bin/tui
--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,28 +1,53 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <experimental/random>
 
 using namespace std;
 using namespace ftxui;
 
-
 int main(int argc, char const *argv[])
 {
-    const string texto = "Hola mundo";
-    Element textElement = text(texto);
+    // const std::string texto = "Hola mundoo";
+    // Element textElement = text(texto);
 
-    Element dibujo = hbox(textElement | border);
+    int fotograma = 0;
+    string reset;
 
-    Dimensions Alto = Dimension::Fixed(10);
-    Dimensions Ancho = Dimension::Fixed(10);
+    while (true)
+    {
+        fotograma++;
 
-    Screen pantalla = Screen::Create(Ancho,Alto);
+        int R = std::experimental::randint(0,255);
+        int G = std::experimental::randint(0,255);
+        int B = std::experimental::randint(0,255);
 
-    Render(pantalla,dibujo);
+        Element personaje = spinner(21, fotograma);
+        Decorator colorFondo = bgcolor(Color::RGB(R,G,B));
+        Decorator colorTexto = color(Color::RGB(R,G,B));
+        Element dibujo = border({
+            hbox(personaje) | colorFondo | colorTexto
+        });
 
-    pantalla.Print();
-    cout<<endl;
+        //Crear dimensiones de pantalla
+        Dimensions Alto = Dimension::Full();
+        Dimensions Ancho = Dimension::Full();
+
+        //Crear pantalla
+        Screen pantalla = Screen::Create(Ancho, Alto);
+
+        // Imprimir en la pantalla
+        Render(pantalla, dibujo);
+        pantalla.Print();
+
+        // Resetear pantalla
+        reset = pantalla.ResetPosition();
+        cout << reset;
+        this_thread::sleep_for(0.1s);
+    }
 
     return 0;
 }
